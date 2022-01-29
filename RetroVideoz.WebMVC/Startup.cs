da3a3183f64d3e49a74f24353cf001b11184a15f
rexmeyer23 <rex_meyer_1426@yahoo.com>
2022-01-29 16:17:50 -0500

    Add project files.

---
+++ RetroVideoz.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RetroVideoz.WebMVC.Startup))]
+namespace RetroVideoz.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
