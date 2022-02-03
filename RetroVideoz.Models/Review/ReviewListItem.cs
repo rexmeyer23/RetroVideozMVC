96009fd512881b916eb1b4e256d362518d5a6b81
rexmeyer23 <rex_meyer_1426@yahoo.com>
2022-02-03 18:27:10 -0500

    review model layer added

--- RetroVideoz.Models/Review/ReviewListItem.cs
+++ RetroVideoz.Models/Review/ReviewListItem.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RetroVideoz.Models.Review
 {
-    internal class ReviewListItem
+    public class ReviewListItem
     {
+        public int ReviewID { get; set; }
+        [Display(Name = "Header")]
+        public string ReviewHeader { get; set; }
+        [Display(Name = "Text")]
+        public string ReviewText { get; set; }
+        [Display(Name = "Star Rating")]
+        public decimal StarRating { get; set; }
+        [Display(Name = "Would Recommend?")]
+        public bool WouldRecommend { get; set; }
     }
 }
--- RetroVideoz.Models/Transaction/TransactionEdit.cs
+++ RetroVideoz.Models/Transaction/TransactionEdit.cs
@@ -8,26 +8,7 @@ namespace RetroVideoz.Models.Transaction
 {
     public class TransactionEdit
     {
-        public int VideoID { get; set; }
-        [Required]
-        public string Title { get; set; }
-        [Required]
-        public string Description { get; set; }
-        [Required]
-        public int Year { get; set; }
-        [Required]
-        public ContentRating Rating { get; set; }
-        [Required]
-        public GenreType Genre { get; set; }
-        [Required]
-        public HomeVideoFormat Format { get; set; }
-        [Required]
-        public decimal Price { get; set; }
-        [ForeignKey(nameof(ApplicationUser))]
-        [Required]
-        public int UserID { get; set; }
-        public virtual ApplicationUser ApplicationUser { get; set; }
-        public byte[] Image { get; set; }
+        
 
         //adding image
 
